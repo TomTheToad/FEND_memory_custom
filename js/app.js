1f40e3e1d997dd2c883804b18b2c65bb09c1861c
TomTheToad <tomthetoad951@gmail.com>
2018-04-23 19:12:36 -0400

    Updated app to check for winning conditions. Added logic to switch css appropriately dependant upon results of a match or failed match. Added temp css classes for each condition. Will need to style animations appropriately.

--- js/app.js
+++ js/app.js
@@ -3,9 +3,8 @@
 function GameItem(name, icon) {
   this.name = name;
   this.icon = icon;
-  this.isMatched = false;
   this.getHTML = function() {
-    return `<div class="card" id="card${this.name}">
+    return `<div class="card" id="${this.name}">
       ${this.icon}
     </div>`;
   }
@@ -13,22 +12,26 @@ function GameItem(name, icon) {
 
 // Fields
 let gridSize = 16;
+let matchCount = 0;
 let gameItems = [];
+
+// selected card
+let previouslySelectedCard;
 
 // TODO: add image sets for themes
 // TODO: only 8 image items, make pairs
 const itemImages = [
-  "item1", "item2", "item3", "item4",
-  "item5", "item6", "item7", "item8",
-  "item9", "item10", "item11", "item12",
-  "item13", "item14", "item15", "item16",
+  "item0", "item1", "item2", "item3",
+  "item4", "item5", "item6", "item7",
+  "item0", "item1", "item2", "item3",
+  "item4", "item5", "item6", "item7",
 ];
 
 // Populate gameItems
 function createGameItems() {
   for (let i = 0; i < gridSize; i++) {
     // create the new gameItems
-    const newGameItem = new GameItem(`item${i}`, itemImages[i]);
+    const newGameItem = new GameItem(`card${i}`, itemImages[i]);
     gameItems.push(newGameItem);
   }
 }
@@ -43,6 +46,7 @@ function createGameBoardHTML() {
 }
 
 // orginal function here: Shuffle function from http://stackoverflow.com/a/2450976
+// This function was a given function in the Udacity starter code.
 function shuffleDeck() {
     let currentIndex = gameItems.length, temporaryValue, randomIndex;
 
@@ -62,10 +66,57 @@ function dealDeck() {
   // console.log(`html: ${html}`);
 }
 
+function checkMatchCount() {
+  if (matchCount === gameItems.length / 2) {
+    console.log("Winner!");
+    return true
+  }
+  return false
+}
+
+function checkSelectedMatch(card) {
+  if (previouslySelectedCard.innerHTML === card.innerHTML) {
+    console.log("We have a match!");
+    previouslySelectedCard.classList.toggle('show');
+    card.classList.toggle('show');
+    matchCount++;
+    checkMatchCount();
+  } else {
+    console.log("No match!");
+    previouslySelectedCard.classList.toggle('open');
+    card.classList.toggle('open');
+  }
+  previouslySelectedCard = undefined;
+}
+
+// TODO: is this better than an anonymous function?
+function cardSelected(card){
+  if (card.classList.contains('show') || checkMatchCount() == true) {
+    return
+  } else if (previouslySelectedCard != undefined && previouslySelectedCard.id != card.id) {
+    card.classList.toggle("open");
+    checkSelectedMatch(card);
+  } else {
+    card.classList.toggle("open");
+    previouslySelectedCard = card;
+  }
+}
+
+function addEventListenersToCards() {
+  for (let i = 0; i < gridSize; i++) {
+    let card = document.querySelector(`#card${i}`);
+    card.addEventListener('click', function(){
+      cardSelected(card);
+    });
+  }
+}
+
 function setUpGameBoard() {
   createGameItems();
   shuffleDeck();
   dealDeck();
+  addEventListenersToCards();
 }
 
 setUpGameBoard();
+// flipCard('#card1');
