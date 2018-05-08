692d71c868feed494595f116a3655cf412fe7e13
TomTheToad <tomthetoad951@gmail.com>
2018-05-08 09:15:52 -0400

    Updated various image urls as they are, apparently, served from a different file location using the atom preview server add on.

--- js/memory-game-item-constructor.js
+++ js/memory-game-item-constructor.js
@@ -3,7 +3,7 @@
   for building the array of items used to build and populate the memory
   game board.
 
-  Please see the README file for overall description of the memory game. 
+  Please see the README file for overall description of the memory game.
  */
 
 function GameItem(id, image) {
@@ -13,10 +13,10 @@ function GameItem(id, image) {
     return `<div class="card-container">
       <div class="card" id="${this.id}">
         <figure class="card-front">
-          <img src="../images/themes/${selectedTheme.cardFront}" alt="top">
+          <img src="images/themes/${selectedTheme.cardFront}" alt="top">
         </figure>
         <figure class="card-back">
-          <img src="../images/themes/${this.image}" alt="hidden">
+          <img src="images/themes/${this.image}" alt="hidden">
         </figure>
       </div>
     </div>`;
--- js/memory-game-theme-functions.js
+++ js/memory-game-theme-functions.js
@@ -15,6 +15,6 @@ function setThemeBackground() {
   let body = document.querySelector('body');
   let bg = selectedTheme.backgroundImage;
   if (body && bg) {
-    body.style.backgroundImage = `url(../images/themes/${bg})`;
+    body.style.backgroundImage = `url(images/themes/${bg})`;
   }
 }
